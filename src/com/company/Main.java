bfb791b1bf0e1729d614ee469f7d1906ca9e1fbc
Yulia_Iakovleva <Yulia_Iakovleva@epam.com>
2022-02-17 20:58:46 +0100

    add lecture notes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,28 @@ public class Main {
         int result = 10 * 12;
         String answer = "The answer is " + result;
         System.out.println(answer);
+
+        String howMany = 20 + " things";
+        System.out.println(howMany);
+
+        // explicit typing does not work
+        // String result2 = 10 + 20;
+
+        // type inference works, but that's number, not string
+         var result2 = 10 + 20;
+
+        // directly convert primitive values
+        int intValue = 42;
+        var fromInt = Integer.toString(intValue);
+        System.out.println(fromInt);
+
+        boolean boolValue = true;
+        var fromBool = Boolean.toString(boolValue);
+        System.out.println(fromBool);
+
+        long longValue = 10_000_000;
+        var fromLong = Long.toString(longValue);
+        System.out.println(fromLong);
+
     }
 }
