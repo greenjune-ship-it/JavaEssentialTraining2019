85316332c8509a46dbea2eb393b2623b92e1acea
Yulia_Iakovleva <Yulia_Iakovleva@epam.com>
2022-02-17 21:23:00 +0100

    add lecture notes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
         var doubleValue = 10_000_000.53;
+
+        var numF = NumberFormat.getNumberInstance();
+        System.out.println("Number: " + numF.format(doubleValue));
+        var intF = NumberFormat.getIntegerInstance();
+
+        intF.setGroupingUsed(false);
+        System.out.println("Number: " + intF.format(doubleValue));
+
+        // formatter can use custom locale
+        var locale = new Locale("de", "DE");
+        var localFormatter = NumberFormat.getNumberInstance(locale);
+        System.out.println("Number: " + localFormatter.format(doubleValue));
+
+        var currencyFormatter = NumberFormat.getCurrencyInstance(locale);
+        System.out.println(currencyFormatter.format(doubleValue));
+
+        var df = new DecimalFormat("$00.00");
+        System.out.println(df.format(1));
+
     }
 }
