3ccd03e7a4d8daaa264b05dab5c945d833b551fa
Yulia_Iakovleva <Yulia_Iakovleva@epam.com>
2022-02-18 16:01:40 +0100

    add lecture notes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        var scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number 1: ");
+        var number1 = scanner.nextInt();
+        System.out.print("Enter a number 2: ");
+        var number2 = scanner.nextInt();
+        System.out.println("Sum is: " + (number1 + number2));
 
     }
 }
