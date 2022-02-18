abafd5d6de921f89c5f392cb12d00d95b3ab130c
Yulia_Iakovleva <Yulia_Iakovleva@epam.com>
2022-02-18 16:26:01 +0100

    add solution for simple calculator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter a numeric value: ");
+        double userNum1 = myScanner.nextDouble();
+        System.out.print("Enter a numeric value: ");
+        double userNum2 = myScanner.nextDouble();
+        System.out.println("The answer is: " + (userNum1 / userNum2));
     }
 }
